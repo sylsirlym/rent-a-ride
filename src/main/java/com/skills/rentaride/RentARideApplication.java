b0b401fd9fa514a9b1c00fe179fbcbd3ff203d6d
Sylvester <sylvester.musyoki@cellulant.com>
2020-02-24 23:09:22 +0300

    Initial commit

---
+++ src/main/java/com/skills/rentaride/RentARideApplication.java
@@ -0,0 +1,13 @@
+package com.skills.rentaride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentARideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentARideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skills/rentaride/RentARideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skills.rentaride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentARideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
