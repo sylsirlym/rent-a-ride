47669eb09425cb7ff59e930b1718c86474ec8527
Sylvester <sylvester.musyoki@cellulant.com>
2020-03-14 13:08:40 +0300

    Add web security config

---
+++ src/main/java/com/skills/rentaride/configs/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.skills.rentaride.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by sylvester
+ * Project rent-a-ride
+ * User: sylvester
+ * Date: 3/13/20
+ * Time: 10:07 PM
+ */
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+
+}
